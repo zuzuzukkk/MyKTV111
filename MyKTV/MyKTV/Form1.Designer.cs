2daf279ec1ee34d4d147c7ffc265163647c2adfc
blank <2543734808@qq.com>
2022-03-07 14:56:26 +0800

    is first pro

---
+++ MyKTV/MyKTV/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿
+namespace MyKTV
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pla = new System.Windows.Forms.Panel();
+            this.lbltime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblname = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.lblfirst = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.pla.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel9.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.lblname);
+            this.panel1.Controls.Add(this.lbltime);
+            this.panel1.Location = new System.Drawing.Point(0, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(2526, 152);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(461, 160);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(2065, 1108);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(12, 1274);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(2514, 314);
+            this.panel3.TabIndex = 0;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pla
+            // 
+            this.pla.Controls.Add(this.panel7);
+            this.pla.Controls.Add(this.panel8);
+            this.pla.Controls.Add(this.panel9);
+            this.pla.Controls.Add(this.panel4);
+            this.pla.Controls.Add(this.panel6);
+            this.pla.Controls.Add(this.panel5);
+            this.pla.Location = new System.Drawing.Point(12, 160);
+            this.pla.Name = "pla";
+            this.pla.Size = new System.Drawing.Size(443, 1108);
+            this.pla.TabIndex = 1;
+            // 
+            // lbltime
+            // 
+            this.lbltime.AutoSize = true;
+            this.lbltime.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbltime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lbltime.Location = new System.Drawing.Point(2198, 48);
+            this.lbltime.Name = "lbltime";
+            this.lbltime.Size = new System.Drawing.Size(178, 72);
+            this.lbltime.TabIndex = 0;
+            this.lbltime.Text = "Time";
+            this.lbltime.Click += new System.EventHandler(this.lbltime_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblname.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lblname.Location = new System.Drawing.Point(51, 36);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(164, 84);
+            this.lblname.TabIndex = 1;
+            this.lblname.Text = "KTV";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.lblfirst);
+            this.panel5.Location = new System.Drawing.Point(6, 6);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(430, 172);
+            this.panel5.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(6, 670);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(430, 191);
+            this.panel6.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.label1);
+            this.panel7.Location = new System.Drawing.Point(6, 222);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(430, 189);
+            this.panel7.TabIndex = 0;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.label2);
+            this.panel8.Location = new System.Drawing.Point(6, 445);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(430, 190);
+            this.panel8.TabIndex = 0;
+            // 
+            // lblfirst
+            // 
+            this.lblfirst.AutoSize = true;
+            this.lblfirst.BackColor = System.Drawing.Color.Transparent;
+            this.lblfirst.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblfirst.ForeColor = System.Drawing.Color.White;
+            this.lblfirst.Location = new System.Drawing.Point(102, 29);
+            this.lblfirst.Name = "lblfirst";
+            this.lblfirst.Size = new System.Drawing.Size(215, 112);
+            this.lblfirst.TabIndex = 0;
+            this.lblfirst.Text = "首页";
+            this.lblfirst.Click += new System.EventHandler(this.lblfirst_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Location = new System.Drawing.Point(6, 670);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(430, 191);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel9
+            // 
+            this.panel9.Controls.Add(this.label5);
+            this.panel9.Location = new System.Drawing.Point(6, 891);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(430, 191);
+            this.panel9.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(28, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 112);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "明星点歌";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(28, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(383, 112);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "字母点歌";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(28, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(383, 112);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "字数点歌";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(28, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(383, 112);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "分类点歌";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(2560, 1600);
+            this.Controls.Add(this.pla);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.pla.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel9.ResumeLayout(false);
+            this.panel9.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbltime;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel pla;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label lblfirst;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MyKTV/MyKTV/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyKTV
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.lbltime.Text = DateTime.Now.ToLongTimeString();
+
+        }
+
+        private void lbltime_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            pla.BackColor = Color.FromArgb(100, Color.Black);
+        }
+
+        private void lblfirst_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
